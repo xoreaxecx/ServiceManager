6e753cdc26cdf95a77f00ff9872a60bfe9ea35df
ecx eax <xoreaxecx@hotmail.com>
2020-05-20 15:24:58 +0300

    Add project files.

---
+++ ServiceManager/Models/ServiceEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceManager.Models
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct ServiceEntry
+    {
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string Name;
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string Description;
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string StatusString;
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string Group;
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string Path;
+        [MarshalAsAttribute(UnmanagedType.BStr)]
+        public string PID;
+    }
+}
